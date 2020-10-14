36fcbf02029aeb8cb23c2a8b45890d34fc43a99a
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-10-14 17:12:58 +0900

    結合テストの実装＋誤字修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,28 +3,24 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
-  def index
-  end
-
   def create
     @room = Room.new(room_params)
     if @room.save
       redirect_to root_path
     else
-      render :new  
+      render :new
     end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
   end
 
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids:[])
   end
-
-  def destory
-    room = Room.find(params)
-    room.destory
-    redirect_to root_path
-  end
-    
 end
